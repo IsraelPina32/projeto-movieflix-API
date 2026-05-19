de2072fb9bfc2f5c54e067651b8b35cb35495679
Israel Coimbra de Pina <israelpina047@gmail.com>
2026-05-19 16:27:58 -0300

    adicionado ordenação, genero e lingua do filme.

--- src/server.ts
+++ src/server.ts
@@ -19,9 +19,17 @@ const prisma = new PrismaClient();
 app.use(express.json());
 
 // ROTA 1: BUSCAR FILMES (GET)
-app.get('/movies', async (req: Request, res: Response): Promise<void> => {
+app.get('/movies', async (_: Request, res: Response): Promise<void> => {
     try {
-        const movies = await prisma.movie.findMany();
+        const movies = await prisma.movie.findMany({
+            orderBy: {
+                title: "asc"
+            },
+            include: {
+                genre: true,  
+                language: true 
+            }
+        });
         res.status(200).json(movies);
     } catch (error) {
         console.error('[DATABASE_ERROR]:', error);
@@ -29,7 +37,6 @@ app.get('/movies', async (req: Request, res: Response): Promise<void> => {
     }
 });
 
-// ROTA 2: ADICIONAR FILME (POST)
 app.post('/movies', async (req: Request, res: Response): Promise<void> => {
     try {
        const { title,  releaseDate, oscar_count, genre_id, language_id } = req.body;
@@ -48,6 +55,34 @@ app.post('/movies', async (req: Request, res: Response): Promise<void> => {
     } catch (error) {
         console.error('[DATABASE_ERROR]:', error);
         res.status(500).json({ error: 'Erro no banco de dados ao adicionar' });
+    }
+});
+
+app.post('/seed', async (_: Request, res: Response): Promise<void> => {
+    try {
+        // 1. Cria o Gênero se não existir
+        const genre = await prisma.genre.upsert({
+            where: { name: 'Ficção Científica' },
+            update: {},
+            create: { name: 'Ficção Científica' }
+        });
+
+        // 2. Cria a Língua se não existir
+        const language = await prisma.language.upsert({
+            where: { name: 'Inglês' },
+            update: {},
+            create: { name: 'Inglês' }
+        });
+
+        // 3. Devolve os IDs direto na tela do Insomnia
+        res.status(200).json({
+            message: "Registros criados com sucesso!",
+            genre_id: genre.id,
+            language_id: language.id
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Erro ao rodar o seed" });
     }
 });
 
